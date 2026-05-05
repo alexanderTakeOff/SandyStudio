ab4d9912e229f5b2d12e88d78585659fcf87d5a8
Alexander Ostrovoy <ostrovoy.alexander@gmail.com>
2026-05-05 20:07:32 +0400

    auto-sync 2026-05-05 20:07

--- webapp/components/preview/AssetPreview.tsx
+++ webapp/components/preview/AssetPreview.tsx
@@ -21,6 +21,8 @@ import { Download, FileWarning, ExternalLink, CloudOff } from 'lucide-react';
 import { fetcher } from '@/lib/swr';
 import { CanonExtensionsPanel } from '@/components/canon/CanonExtensionsPanel';
 import type { CanonExtensionProposal } from '@/lib/api/canon-extensions';
+import { isAnimaticV1, type AnimaticContract } from '@/lib/api/animatic-shotlist';
+import { AnimaticPlayer } from '@/components/animatic/AnimaticPlayer';
 
 const TEXT_PREFIXES = ['SCR', 'STB', 'BIB', 'PRO', 'REV', 'SPC', 'STA', 'SBL'];
 
